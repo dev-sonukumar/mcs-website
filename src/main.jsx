12e9043617c665db9f59dd5a932092454cc3d16b
sonu kumar <sonu638056@gmail.com>
2025-02-11 21:24:57 +0530

    homr pc

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import NotFound from "./Pages/NotFound.jsx";
 // import Bis from "./Pages/Bis.jsx";
 import Home from "./Pages/Home.jsx";
 import Footer from "./Footer.jsx";
-import Post from "./components/posts/post.jsx";
+// import Post from "./components/posts/post.jsx";
 import FAQ from "./Pages/Faq.jsx";
 import ContactUs from "./Pages/ContactUS.jsx";
 
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> }, // Default route
       { path: "/", element: <Home /> },
       { path: "services", element: <Services /> },
-      { path: "services/:post", element: <Post /> },
+      // { path: "services/:post", element: <Post /> },
       { path: "faqs", element: <FAQ /> },
       { path: "contact", element: <ContactUs /> },
       { path: "*", element: <NotFound /> }, // Catch-all route
