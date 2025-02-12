289e7ba5e26e20f29f6326f4bbbc1b99738d2c0f
sonu kumar <sonu638056@gmail.com>
2025-02-12 15:46:41 +0530

    office

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/faq": {
+        target: "https://mcs-backend-b5kl.onrender.com",
+        changeOrigin: true,
+        secure: false, // Required if Render uses HTTPS
+      },
+    },
+  },
+}); 
